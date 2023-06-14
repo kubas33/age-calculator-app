c42e1618bb185326d8331a538d95dfe35490fb4a
kubas33 <Stasiak91@gmail.com>
2023-06-14 14:00:07 +0200

    Age calculator v2

--- script.js
+++ script.js
@@ -35,19 +35,51 @@ function handleInput() {
 
 }
 
+function calculateLeapYears(calcYears) {
+    let leapYears = 0;
+    for (let i = year; i < calcYears; i--) {
+        if (isLeapYear(i))
+            leapYears++;
+    }
+    if (date.getMonth() > month || (date.getMonth() === month && date.getDate() > day)) {
+        leapYears--;
+    }
+    return leapYears;
+}
+
 function Calculate() {
     inputUpdate();
     today = new Date();
     console.log("today " + today);
     console.log("date " + date);
-    let diff = new Date(Math.floor(today - date));
+    //let diff = new Date(Math.floor(today - date));
+    let diff = today.getTime() - date.getTime();
+    let age = new Date(diff);
+    var calcYears = age.getUTCFullYear() - 1970;
+    var calcMonths = age.getUTCMonth();
+    var calcDays = age.getUTCDate() - 1;
+    var leapYears = calculateLeapYears(calcYears);
+    var leapDays = leapYears * 366;
+    var notLeapDays = (calcYears - leapYears) * 365;
+    calcDays -= (notLeapDays + leapDays);
+
+
+    //today.setTime(today.getTime() - diff);
+    /*
+    diff.setFullYear(diff.getFullYear() - 1970);
     console.log(diff.getTime() / milisecInDay);
     let calcHours = diff / 3600;
     //let calcDays = Math.floor(diff.getTime() / milisecInDay);
     let calcDays = diff.getDate() - 1;
-    //let calcYears = year - yearValue;
-    let calcYears = diff.getFullYear() - 1970;
-    let calcMonths = diff.getMonth();
+    //let calcYears = diff.getFullYear();
+    //let calcDays = day - date.getDate();
+    let calcYears = year - date.getFullYear();
+    //let calcMonths = diff.getMonth();
+    */
+    
+   //let calcDays = today.getDate();
+   //let calcMonths = today.getMonth() + 1;
+   //let calcYears = today.getFullYear();
     console.log(calcYears + " lat " + calcMonths + " miesięcy " + calcDays + " dni " );
     document.getElementById("calc-years").textContent = calcYears;
     document.getElementById("calc-months").textContent = calcMonths;
